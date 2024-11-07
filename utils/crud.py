ee1d0296951fa7e86e04720882940d33bf13a0d9
newyearoldme <beddeb29@gmail.com>
2024-11-07 20:13:02 +0200

    новый crud

--- utils/crud.py
+++ utils/crud.py
@@ -1,11 +1,13 @@
+from sqlalchemy import select
 from .models import AdminLog
 from utils.db_alchemy import get_db
 
 
 def get_user_messages() -> list[AdminLog]:
     db = next(get_db())
-    result = db.query(AdminLog).filter(
+    statement = select(AdminLog).where(
         AdminLog.type == 61,
         (AdminLog.message.like('%say from%') | AdminLog.message.like('%whisper from%'))
-    ).all()
+    )
+    result = db.scalars(statement).all()
     return result
